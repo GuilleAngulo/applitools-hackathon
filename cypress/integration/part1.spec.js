5ad00a1414c1aef80063402338b437bc266ceec3
Guillermo Angulo <anyermo@gmail.com>
2020-12-10 21:52:50 -0300

    First commit

---
+++ cypress/integration/part1.spec.js
@@ -0,0 +1,62 @@
+const url = "https://demo.applitools.com/tlcHackathonMasterV1.html"
+
+const config = {
+  appName: 'AppliFashion',
+  batchName: 'Holiday Shopping',
+  browser: { width: 1200, height: 800, name: "chrome" },
+}
+
+describe(`Part 1`, () => {
+
+    beforeEach(() => {
+      cy.visit(url);
+    });
+
+    afterEach(() => {
+      cy.eyesClose();
+    });
+
+    it('Test 1', function () {
+
+        cy.eyesOpen({ ...config, testName: this.test.title })
+        
+        cy.eyesCheckWindow({
+            tag: "main page",
+            target: 'window',
+            fully: true
+        });
+
+    });
+
+    it('Test 2', function () {
+
+        cy.eyesOpen({ ...config, testName: this.test.title })
+
+        cy.get('#filter_1').find('li').contains('Black').click();
+        cy.get('#filterBtn').click();
+
+        cy.eyesCheckWindow({
+            target: 'region',
+            selector: '#product_grid',
+            tag: "filter by color",
+          });
+
+        cy.get('#product_grid').children().should('have.length', 2)
+
+    });
+
+    it(`Test 3`, function () {
+
+      cy.eyesOpen({ ...config, testName: this.test.title })
+
+      cy.get('h3').contains('Appli Air x Night').click();
+      
+      cy.eyesCheckWindow({
+        tag: "product details",
+        target: 'window',
+        fully: true
+      });
+
+    });
+
+});
